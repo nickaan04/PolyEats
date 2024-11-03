da57a92e1b8643853d604fff5e21711cf78ae43d
njahadi <njahadi@calpoly.edu>
2024-11-03 02:28:40 -0800

    sprint 1 demo

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,19 +39,93 @@ app.get("/complexes", (req, res) => {
     });
 });
 
-//get all restaurants within a specific complex
+//get all restaurants within a specific complex (with filters/sorting if desired)
 app.get("/complexes/:complexId/restaurants", (req, res) => {
   const complexId = req.params.complexId;
+  const {
+    name,
+    minRating,
+    cuisine,
+    delivery,
+    price,
+    accepted_payments,
+    nutrition_types,
+    hours,
+    sortField,
+    sortOrder
+  } = req.query;
+
+  const filters = {};
+  if (name) {
+    //e.g. ?name=Hearth or ?name=hearth (not case sensitive)
+    filters.name = name;
+  }
+  if (minRating) {
+    //e.g. ?minRating=4
+    filters.avg_rating = parseFloat(minRating);
+  }
+  if (cuisine) {
+    //e.g. ?cuisine=Mexican or ?name=mexican (not case sensitive)
+    filters.cuisine = cuisine;
+  }
+  if (delivery) {
+    //e.g. ?delivery=true
+    filters.delivery = delivery === "true";
+  }
+  if (price) {
+    //e.g. ?price=$
+    filters.price = price;
+  }
+  if (accepted_payments) {
+    //JSON object, e.g., ?accepted_payments={"PolyCard": true, "CreditDebit": true}
+    try {
+      filters.accepted_payments = JSON.parse(accepted_payments);
+    } catch (error) {
+      return res
+        .status(400)
+        .send({ error: "Invalid format for accepted_payments" });
+    }
+  }
+  if (nutrition_types) {
+    //JSON object, e.g., ?nutrition_types={"Vegan": true, "GlutenFree": true}
+    try {
+      filters.nutrition_types = JSON.parse(nutrition_types);
+    } catch (error) {
+      return res
+        .status(400)
+        .send({ error: "Invalid format for nutrition_types" });
+    }
+  }
+  if (hours) {
+    //JSON object, e.g., ?hours={"M": "7:30AM-3:30PM"}
+    try {
+      filters.hours = JSON.parse(hours);
+    } catch (error) {
+      return res.status(400).send({ error: "Invalid format for hours" });
+    }
+  }
 
   restaurantService
-    .getRestaurantsByComplex(complexId)
+    .getRestaurants(filters, sortField, sortOrder || "asc", complexId)
     .then((restaurants) => {
       res.status(200).send({ restaurants_list: restaurants });
     })
     .catch((error) => {
-      res.status(500).send({ error: "Error fetching restaurants in complex" });
+      res
+        .status(500)
+        .send({
+          error: "Error fetching/filtering/sorting restaurants in complex"
+        });
     });
 });
+/* For sorting:
+  ?sortField=name&sortOrder=asc --> Name A-Z
+  ?sortField=name&sortOrder=desc --> Name Z-A
+  ?sortField=avg_rating&sortOrder=desc --> Average Rating high to low
+  ?sortField=avg_rating&sortOrder=asc --> Average Rating low to high
+  ?sortField=price&sortOrder=asc --> price low to high
+  ?sortField=price&sortOrder=desc --> price high to low
+  */
 
 //get specific restaurant by id
 app.get("/restaurant/:id", (req, res) => {
@@ -68,67 +142,5 @@ app.get("/restaurant/:id", (req, res) => {
     })
     .catch((error) => {
       res.status(500).send({ error: "Error fetching restaurant" });
-    });
-});
-
-//get restaurants that satisfy specific filters
-app.get("/restaurants/filter", (req, res) => {
-  const { minRating, cuisine, delivery, accepted_payments, nutrition_types } =
-    req.query;
-
-  const filters = {};
-  if (minRating) {
-    filters.avg_rating = parseFloat(minRating);
-  }
-  if (cuisine) {
-    filters.cuisine = cuisine;
-  }
-  if (delivery) {
-    filters.delivery = delivery === "true";
-  }
-  if (accepted_payments) {
-    try {
-      //parse accepted_payments from query string to an object
-      filters.accepted_payments = JSON.parse(accepted_payments);
-    } catch (error) {
-      return res
-        .status(400)
-        .send({ error: "Invalid format for accepted_payments" });
-    }
-  }
-  if (nutrition_types) {
-    try {
-      //parse nutrition_types from query string to an object
-      filters.nutrition_types = JSON.parse(nutrition_types);
-    } catch (error) {
-      return res
-        .status(400)
-        .send({ error: "Invalid format for nutrition_types" });
-    }
-  }
-
-  restaurantService
-    .filterRestaurants(filters)
-    .then((restaurants) => {
-      res.status(200).send({ restaurants_list: restaurants });
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .send({ error: "Error fetching restaurants with specified filters" });
-    });
-});
-
-//get restaurants sorted by a specific parameter
-app.get("/restaurants/sort", (req, res) => {
-  const { sortField, sortOrder } = req.query;
-
-  restaurantService
-    .getSortedRestaurants(sortField, sortOrder || "asc")
-    .then((restaurants) => {
-      res.status(200).send({ restaurants_list: restaurants });
-    })
-    .catch((error) => {
-      res.status(500).send({ error: "Error sorting restaurants" });
     });
 });
--- packages/express-backend/services/restaurant-service.js
+++ packages/express-backend/services/restaurant-service.js
@@ -1,27 +1,34 @@
 import restaurantModel from "../models/restaurant.js";
 
-//get restaurants within a specific complex
-function getRestaurantsByComplex(complexId) {
-  return restaurantModel.find({ complex_id: complexId }); //populate reviews?
-}
-
 //get a single restaurant by ID
 function getRestaurantById(id) {
   return restaurantModel.findById(id); //populate reviews?
 }
 
-//filter restaurants that match given criteria
-function filterRestaurants(filters) {
-  const query = {};
+//filter and/or sort restaurants in complex based on given criteria
+function getRestaurants(filters, sortField, sortOrder = "asc", complexId) {
+  const query = complexId ? { complex_id: complexId } : {};
+  const sortOptions = { [sortField]: sortOrder === "asc" ? 1 : -1 };
 
+  if (filters.name) {
+    //case-insensitive name search
+    query.name = { $regex: filters.name, $options: "i" };
+  }
   if (filters.avg_rating) {
-    query.avg_rating = { $gte: filters.avg_rating }; //returns restaurants greater than or equal to specified rating
+    //searches for restaurant with average rating greater than or equal to specified minRating
+    query.avg_rating = { $gte: filters.avg_rating };
   }
   if (filters.cuisine) {
-    query.cuisine = filters.cuisine; //returns restaurants with specific cuisine
+    //case-insensitive cuisine search
+    query.cuisine = { $regex: filters.cuisine, $options: "i" };
   }
   if (filters.delivery) {
-    query.delivery = filters.delivery; //returns restaurants based on if they offer delivery
+    //searches for restaurants that offer delivery
+    query.delivery = filters.delivery;
+  }
+  if (filters.price) {
+    //searches for restaurants that meet the desired price
+    query.price = filters.price;
   }
   if (filters.accepted_payments) {
     //returns restaurants based on the desired accepted payments
@@ -37,20 +44,17 @@ function filterRestaurants(filters) {
         filters.nutrition_types[nutritionType];
     });
   }
+  if (filters.hours) {
+    //returns restaurants based on the desired hours
+    Object.keys(filters.hours).forEach((day) => {
+      query[`hours.${day}`] = { $regex: filters.hours[day], $options: "i" };
+    });
+  }
 
-  return restaurantModel.find(query);
-}
-
-//sort restaurants based on the field (ascending or descending)
-function getSortedRestaurants(sortField, sortOrder = "asc") {
-  const sortOptions = {};
-  sortOptions[sortField] = sortOrder === "asc" ? 1 : -1;
-  return restaurantModel.find().sort(sortOptions);
+  return restaurantModel.find(query).sort(sortOptions);
 }
 
 export default {
   getRestaurantById,
-  getRestaurantsByComplex,
-  getSortedRestaurants,
-  filterRestaurants
+  getRestaurants
 };
